d9fa345cc0333e40e51a915e8edc323ea71f00b5
balits <balits.peter@gmail.com>
2024-03-15 22:17:18 +0100

    Add docker-compose exec

--- scripts/rundb.sh
+++ scripts/rundb.sh
@@ -1,1 +1,2 @@
 docker run --name konyvtar-db -e POSTGRES_USER=petiyeti -e POSTGRES_PASSWORD=petiyeti -e POSTGRES_DB=elte_felho_konyvek -v konyvtar-db:/var/lib/postgresql/data -p 5432:5432 -d postgres
+
