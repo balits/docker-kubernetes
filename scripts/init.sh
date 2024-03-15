f1f256ae62cdcc716fd7c0b1d5e51c30893292b3
balits <balits.peter@gmail.com>
2024-03-15 22:14:35 +0100

    Add docker-compose exec

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,1 +1,5 @@
-cat db\backup.tar | docker exec -i konyvtar-db pg_restore -U petiyeti -d elte_felho_konyvek --format=t
+# for a single image
+# cat db/backup.tar | docker exec -i konyvtar-db pg_restore -U petiyeti -d elte_felho_konyvek --format=t
+
+# using docker-compose,
+# docker-compose exec -T <service> pg_restore -U <username> -d <db-name> --format=t < path/to/tarball
